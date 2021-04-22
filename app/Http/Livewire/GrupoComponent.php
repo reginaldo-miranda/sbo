b18a044f184ab7b6920e4f1c160548e0f524aad5
Reginaldo Miranda <reginaldobrain@gmail.com>
2021-04-22 17:25:00 -0300

    arquivos

---
+++ app/Http/Livewire/GrupoComponent.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class GrupoComponent extends Component
+{
+    public function render()
+    {
+        return view('livewire.grupo-component');
+    }
+}
---
+++ resources/views/adm.blade.php
@@ -0,0 +1,5 @@
+@extends('app')
+<h1>adm</h1>
+<div>
+    @livewire('grupo-component')
+</div>
